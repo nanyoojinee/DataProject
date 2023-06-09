38f6604e106d7948aa909af7e02affbd432a8de6
[AI7] EUNSIL LEE <eunsilling@gmail.com>
2023-06-09 20:21:35 +0900

    🐛 Fix :  delete actCategory 인한 오류 수정

--- backend/src/services/activityService.js
+++ backend/src/services/activityService.js
@@ -1,7 +1,7 @@
 import { Activity } from '../db/models/Activity.js';
 
 class activityService {
-  // create/ post
+  //
   static async getAllActivity({ groupId, userId, name, usedDate, state, actCategoryId }) {
     const activityUser = {
       groupId,
